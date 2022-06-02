df1f140d425f0d8eea6d54e00a4ef20eeb0845f7
Andrei <gelserkirchen@mail.ru>
2022-06-02 06:36:57 +0300

    fix bugs

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -20,7 +20,7 @@ function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
     return (
         <PopupWithForm name={"update-avatar"} title={"Обновить аватар"} buttonText={"Сохранить"} isOpen={isOpen} onClose={onClose} onSubmit={handleSubmit}>
             <label htmlFor="link-input" className="popup__label">
-                <input id="update-avatar-link" defaultValue={ avatarRef.current } ref={avatarRef} name="link" className="popup__input popup__input_type_link"
+                <input id="update-avatar-link" defaultValue={avatarRef.current} ref={avatarRef} name="link" className="popup__input popup__input_type_link"
                     type="url" placeholder="Ссылка на аватар" required minLength="2" />
                 <span className="popup__input-error update-avatar-link-error">Текст ошибки</span>
             </label>
